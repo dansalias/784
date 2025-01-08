afa2b6cbeeaa2d6b4ffc52df5cfc39c5180040b1
dansalias <danyoungnz@gmail.com>
2025-01-08 14:44:55 -0500

    (website): add link to source code

--- website/components/App.vue
+++ website/components/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div
     class="
-      grid min-h-dvh pt-4 pb-12
+      grid min-h-dvh py-4
       bg-linear-135 from-cyan-300 to-emerald-100 bg-fixed
       text-center text-indigo-900
     ">
     <div class="m-auto">
       <h1 class="mb-8 font-display text-6xl">784</h1>
       <app-demo />
+      <p class="mt-12 text-sm">
+        <a
+          class="underline decoration-dotted"
+          href="https://github.com/dansalias/784"
+          target="_blank"
+        >
+          Source on GitHub
+        </a>
+      </p>
     </div>
   </div>
 </template>
