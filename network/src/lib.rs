35b8d77f7be387c337bf805b18bc5e20e9770b3e
dansalias <danyoungnz@gmail.com>
2025-01-23 16:13:39 -0500

    (network): remove redundant bprop

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -74,75 +74,10 @@ impl Network {
                 activations
             })
     }
-
-    pub fn bprop(&mut self, error: &[f64], learning_rate: f64) {
-        self
-            .layers
-            .iter()
-            .enumerate()
-            .zip(
-                self
-                    .activations[..self.activations.len() - 1]
-                    .iter()
-                    .zip(self.activations[1..].iter())
-            )
-            .rfold(
-                error.to_vec(),
-                |
-                    gradient,
-                    ((layer_index, (_, activation_function)), (
-                        (_, input_activations),
-                        (output_weighted_sums, _),
-                    ))
-                | {
-                    let gradient: Vec<f64> =
-                        gradient
-                            .iter()
-                            .zip((
-                                match activation_function {
-                                    Some(af) => af.bprop(&output_weighted_sums),
-                                    _ => vec![1.0; output_weighted_sums.len()],
-                                }
-                            ).iter())
-                            .map(|(da, dz)| da * dz)
-                            .collect();
-
-                    let next_gradient = (0..input_activations.len())
-                        .map(|i|
-                            gradient
-                                .iter()
-                                .zip(self
-                                    .parameters[layer_index]
-                                    .iter()
-                                    .map(|(weights, _)| weights[i])
-                                )
-                                .map(|(g, w)| g * w)
-                                .sum::<f64>()
-                        )
-                        .collect::<Vec<f64>>();
-
-                    self.parameters[layer_index] =
-                        self.parameters[layer_index]
-                            .iter()
-                            .zip(gradient)
-                            .map(|((weights, bias), g)| (
-                                weights
-                                    .iter()
-                                    .zip(input_activations)
-                                    .map(|(w, a)| w + g * a * learning_rate)
-                                    .collect(),
-                                bias + g * learning_rate,
-                            ))
-                            .collect();
-
-                    next_gradient
-            });
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::util::test;
 
     #[test]
@@ -150,29 +85,6 @@ mod tests {
         assert_eq!(
             test::get_test_network().fprop(&[-1.0, 1.0, 1.0]),
             vec![1.5, 2.5],
-        );
-    }
-
-    #[test]
-    fn propagates_backward() {
-        let mut network = test::get_test_network();
-
-        network.fprop(&[-1.0, 1.0, 1.0]);
-
-        network.bprop(&[1.0, 0.5], 1.0);
-
-        assert_eq!(
-            network.parameters,
-            vec![
-                vec![
-                    (vec![-0.5, 2.0, 2.0], 1.5),
-                    (vec![1.0, 0.0, 0.0], 0.5),
-                ],
-                vec![
-                    (vec![2.5, 1.0], 1.5),
-                    (vec![2.0, 1.0], 1.0),
-                ],
-            ],
         );
     }
 }
